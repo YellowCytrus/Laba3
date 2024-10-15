664fc3720b5fcf70fad35f93199a1bdd91e7562c
Cytrus <LancasterProxima@ya.ru>
2024-10-15 19:54:34 +0700

    Добавил с видео

---
+++ лаба2 Медиа/Laba2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace лаба2_Медиа
+{
+    partial class Laba2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Laba2));
+            this.open = new System.Windows.Forms.Button();
+            this.files = new System.Windows.Forms.ListBox();
+            this.play = new System.Windows.Forms.Button();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.FlatAppearance.BorderSize = 0;
+            this.open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.open.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.open.Location = new System.Drawing.Point(12, 12);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(202, 72);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // files
+            // 
+            this.files.FormattingEnabled = true;
+            this.files.ItemHeight = 16;
+            this.files.Location = new System.Drawing.Point(12, 121);
+            this.files.Name = "files";
+            this.files.Size = new System.Drawing.Size(641, 676);
+            this.files.TabIndex = 1;
+            // 
+            // play
+            // 
+            this.play.FlatAppearance.BorderSize = 0;
+            this.play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.play.Location = new System.Drawing.Point(220, 12);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(202, 72);
+            this.play.TabIndex = 3;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(659, 121);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(612, 545);
+            this.player.TabIndex = 2;
+            // 
+            // Laba2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1395, 803);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.files);
+            this.Controls.Add(this.open);
+            this.Name = "Laba2";
+            this.Text = "Lab5";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.ListBox files;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Button play;
+    }
+}
+
---
+++ лаба2 Медиа/Laba2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TagLib.WavPack;
+
+namespace лаба2_Медиа
+{
+    public partial class Laba2 : Form
+    {
+        public Laba2()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            dialog.InitialDirectory = "c:\\";
+            dialog.Filter = "Windows Media Video (*.wmv)|*.wmv|All files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var filename = dialog.FileName;
+                TagLib.File file = TagLib.File.Create(filename);
+
+                var db = new AeroDataContext();
+                var data = new Videos
+                {
+                    FileName = filename,
+                    Width = file.Properties.VideoWidth,
+                    Heigth = file.Properties.VideoHeight,
+
+                };
+                db.Videos.InsertOnSubmit(data);
+                db.SubmitChanges();
+
+                files.Items.Add(filename);
+
+                player.URL = filename;
+                player.close();
+            }
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            var playlist = player.newPlaylist("Playlist", "");
+
+            foreach (var selected in files.Items)
+            {
+                string s = selected.ToString();
+                var temp = player.newMedia(s);
+                playlist.appendItem(temp);
+            }
+        }
+    }
+}
