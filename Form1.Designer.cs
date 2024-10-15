21eb61276529b5c06ea6f862186fcac81748451d
Cytrus <LancasterProxima@ya.ru>
2024-10-15 19:28:57 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Лаба3_Медиа
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.open = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.year = new System.Windows.Forms.TextBox();
+            this.artist = new System.Windows.Forms.TextBox();
+            this.album = new System.Windows.Forms.TextBox();
+            this.comment = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aeroDataSet = new Лаба3_Медиа.aeroDataSet();
+            this.audioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.audioTableAdapter = new Лаба3_Медиа.aeroDataSetTableAdapters.AudioTableAdapter();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.singerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.albumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.commentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filenameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aeroDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.audioBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.FlatAppearance.BorderSize = 0;
+            this.open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.open.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.open.Location = new System.Drawing.Point(12, 12);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(181, 66);
+            this.open.TabIndex = 0;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // play
+            // 
+            this.play.FlatAppearance.BorderSize = 0;
+            this.play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.play.Location = new System.Drawing.Point(199, 12);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(181, 66);
+            this.play.TabIndex = 1;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // stop
+            // 
+            this.stop.FlatAppearance.BorderSize = 0;
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stop.Location = new System.Drawing.Point(386, 12);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(181, 66);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.Location = new System.Drawing.Point(12, 104);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(974, 40);
+            this.title.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // year
+            // 
+            this.year.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.year.Location = new System.Drawing.Point(12, 280);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(974, 40);
+            this.year.TabIndex = 5;
+            // 
+            // artist
+            // 
+            this.artist.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.artist.Location = new System.Drawing.Point(12, 162);
+            this.artist.Name = "artist";
+            this.artist.Size = new System.Drawing.Size(974, 40);
+            this.artist.TabIndex = 6;
+            // 
+            // album
+            // 
+            this.album.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.album.Location = new System.Drawing.Point(12, 221);
+            this.album.Name = "album";
+            this.album.Size = new System.Drawing.Size(974, 40);
+            this.album.TabIndex = 7;
+            // 
+            // comment
+            // 
+            this.comment.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comment.Location = new System.Drawing.Point(12, 341);
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(974, 40);
+            this.comment.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.titleDataGridViewTextBoxColumn,
+            this.singerDataGridViewTextBoxColumn,
+            this.albumDataGridViewTextBoxColumn,
+            this.commentDataGridViewTextBoxColumn,
+            this.filenameDataGridViewTextBoxColumn,
+            this.yearDataGridViewTextBoxColumn,
+            this.idDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.audioBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 404);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(974, 229);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // aeroDataSet
+            // 
+            this.aeroDataSet.DataSetName = "aeroDataSet";
+            this.aeroDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // audioBindingSource
+            // 
+            this.audioBindingSource.DataMember = "Audio";
+            this.audioBindingSource.DataSource = this.aeroDataSet;
+            // 
+            // audioTableAdapter
+            // 
+            this.audioTableAdapter.ClearBeforeFill = true;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // singerDataGridViewTextBoxColumn
+            // 
+            this.singerDataGridViewTextBoxColumn.DataPropertyName = "Singer";
+            this.singerDataGridViewTextBoxColumn.HeaderText = "Singer";
+            this.singerDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.singerDataGridViewTextBoxColumn.Name = "singerDataGridViewTextBoxColumn";
+            this.singerDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // albumDataGridViewTextBoxColumn
+            // 
+            this.albumDataGridViewTextBoxColumn.DataPropertyName = "Album";
+            this.albumDataGridViewTextBoxColumn.HeaderText = "Album";
+            this.albumDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.albumDataGridViewTextBoxColumn.Name = "albumDataGridViewTextBoxColumn";
+            this.albumDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // commentDataGridViewTextBoxColumn
+            // 
+            this.commentDataGridViewTextBoxColumn.DataPropertyName = "Comment";
+            this.commentDataGridViewTextBoxColumn.HeaderText = "Comment";
+            this.commentDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.commentDataGridViewTextBoxColumn.Name = "commentDataGridViewTextBoxColumn";
+            this.commentDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // filenameDataGridViewTextBoxColumn
+            // 
+            this.filenameDataGridViewTextBoxColumn.DataPropertyName = "Filename";
+            this.filenameDataGridViewTextBoxColumn.HeaderText = "Filename";
+            this.filenameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.filenameDataGridViewTextBoxColumn.Name = "filenameDataGridViewTextBoxColumn";
+            this.filenameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // yearDataGridViewTextBoxColumn
+            // 
+            this.yearDataGridViewTextBoxColumn.DataPropertyName = "Year";
+            this.yearDataGridViewTextBoxColumn.HeaderText = "Year";
+            this.yearDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.yearDataGridViewTextBoxColumn.Name = "yearDataGridViewTextBoxColumn";
+            this.yearDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 645);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comment);
+            this.Controls.Add(this.album);
+            this.Controls.Add(this.artist);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aeroDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.audioBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox year;
+        private System.Windows.Forms.TextBox artist;
+        private System.Windows.Forms.TextBox album;
+        private System.Windows.Forms.TextBox comment;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private aeroDataSet aeroDataSet;
+        private System.Windows.Forms.BindingSource audioBindingSource;
+        private aeroDataSetTableAdapters.AudioTableAdapter audioTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn singerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn albumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn commentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn filenameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace Лаба3_Медиа
+{
+    public partial class Form1 : Form
+    {
+        private string filename;
+        private MediaPlayer player = new MediaPlayer();
+        private AeroDataContext db = new AeroDataContext();
+
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = "c:\\";
+            dialog.Filter = "Windows Media Audio (*.wma)|*.wma|All files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            dialog.ShowDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                filename = dialog.FileName;
+                TagLib.File file = TagLib.File.Create(filename);
+
+                var db = new AeroDataContext();
+                var data = new Audio
+                {
+                    Filename = filename,
+                    Title = file.Tag.Title,
+                    Album = file.Tag.Album,
+                    Year = (int)file.Tag.Year,
+                    Comment = file.Tag.Comment
+                };
+                db.Audio.InsertOnSubmit(data);
+                db.SubmitChanges();
+
+                title.Text = file.Tag.Title;
+                artist.Text = file.Tag.FirstPerformer;
+                album.Text = file.Tag.Album;
+                year.Text = file.Tag.Year.ToString();
+                comment.Text = file.Tag.Comment;
+            }
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("file://" + filename);
+            player.Open(uri);
+            player.Play();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            player.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aeroDataSet.Audio". При необходимости она может быть перемещена или удалена.
+            this.audioTableAdapter.Fill(this.aeroDataSet.Audio);
+
+        }
+    }
+}
